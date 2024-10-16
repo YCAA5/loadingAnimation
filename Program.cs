c3fe3446abf14ef10959efbc99808bc401cb1b1a
Murat Abbaslı <allahverdi.abbasli.1995@gmail.com>
2024-10-16 10:33:33 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿// * = buradaki değerler değiştirilebilir
+using System;
+
+namespace nitelikler 
+{
+    public class Program 
+    {
+        public static void Main(string[] args)
+        {
+            
+        for (int i = 0; i < 10; i++) // * 
+        {
+            Thread.Sleep(100); // *
+            bool kontrol = true;
+            while (kontrol)
+                {
+                    Console.Write("\rYükleniyor|");
+                    Thread.Sleep(175);
+                    Console.Write("\r");
+                    Console.Write("\rYükleniyor/");
+                    Thread.Sleep(175);
+                    Console.Write("\r");
+                    Console.Write("Yükleniyor-");
+                    Thread.Sleep(175);
+                    Console.Write("\r");
+                    Console.Write("Yükleniyor\\");
+                    Thread.Sleep(175);
+                    kontrol = false;
+                }
+        }
+
+        Console.WriteLine("\nAnimasyon durdu.");
+        Console.ReadLine();  
+        }
+    }
+}
